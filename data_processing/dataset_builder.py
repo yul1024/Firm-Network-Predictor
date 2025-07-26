534783ba336bcf7ee6347f36e7e1d543d1f246c0
yu <yul1024@outlook.com>
2025-07-26 20:25:04 +0800

    Create dataset_builder.py

---
+++ data_processing/dataset_builder.py
@@ -0,0 +1,21 @@
+"""
+构建训练和测试的
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class DatasetBuilder:
+    @staticmethod
+    def build_train_val_dataset(
+
+    ):
+        ...
+
+    @staticmethod
+    def build():
+        ...
+
