2fee93d4e356105895661c3a53abe31fc73e9b82
yu <yul1024@outlook.com>
2025-07-26 20:25:10 +0800

    Create pre_analysis_scripts.py

---
+++ data_processing/pre_analysis_scripts.py
@@ -0,0 +1,52 @@
+"""
+简单的进行分析的脚本。
+"""
+
+from __future__ import annotations
+
+import pandas as pd
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class SomePreAnalysisMethods:
+    @staticmethod
+    def check_df(
+        df_path: str | Path,
+    ):
+        df = pd.read_json(df_path, lines=True)
+        print(df.info())
+        """
+        Columns: 444 entries
+        """
+        print(df.dtypes)
+        """
+        dtypes: float64(424), int64(16), object(4)
+        """
+        print(df.select_dtypes(include=['object']))
+        """
+        size_grp
+        array(['micro', 'nano', 'large', None, 'mega', 'small'], dtype=object)
+        iid
+        array(['01', None, '00X', '02', 1.0, 2.0, '03', 3.0], dtype=object)
+        bidask
+        array([1, 0])
+        excntry
+        array(['USA'], dtype=object)
+        curcd
+        array(['USD'], dtype=object)
+        """
+
+    @staticmethod
+    def check_date(
+        df_path: str | Path,
+    ):
+        df = pd.read_json(df_path, lines=True)
+        print(type(df.loc[0, 'date']))
+        """
+        numpy.int64
+        """
+
+
