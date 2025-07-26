cfc59f2d988fab4df86d48623e9a55ba0534ed79
yu <yul1024@outlook.com>
2025-07-26 21:43:19 +0800

    Create lightning_trainer.py

---
+++ src/trainer/lightning_trainer.py
@@ -0,0 +1,18 @@
+"""
+基于lightning构建的trainer。
+"""
+
+from __future__ import annotations
+
+import torch
+import lightning as pl
+from pathlib import Path
+from omegaconf import OmegaConf
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class LightningTrainer:
+    ...
+
