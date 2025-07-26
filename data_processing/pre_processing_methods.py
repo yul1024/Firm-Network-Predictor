1134d4343d6f6e95e42ef6f1bda7e64f07d6b487
yu <yul1024@outlook.com>
2025-07-26 21:43:12 +0800

    Update pre_processing_methods.py

--- data_processing/pre_processing_methods.py
+++ data_processing/pre_processing_methods.py
@@ -16,8 +16,8 @@ class PreProcessingMethods:
     # ====主要方法。====
     @staticmethod
     def split_csv_to_jsonl_files(
-        path_to_csv: str | Path,
-        dir_to_save: str | Path,
+        csv_file_path: str | Path,
+        results_dir: str | Path,
     ) -> None:
         """
         将最初始很大的csv文件处理为多个容易操作的jsonl文件。
@@ -25,24 +25,39 @@ class PreProcessingMethods:
         构建该方法的原因是原始文件太大，并且部分字段存在问题。
 
         Args:
-            path_to_csv (Union[str, Path]): 原始csv文件的路径。
-            dir_to_save (Union[str, Path]): 要保存处理结果的文件夹。
+            csv_file_path (Union[str, Path]): 原始csv文件的路径。
+            results_dir (Union[str, Path]): 要保存处理结果的文件夹。
 
         Returns:
             None: 保存jsonl文件至指定文件夹。
                 即使性能好的机器，这一步也要花半小时。
         """
-        dir_to_save = Path(dir_to_save)
-        for i, chunk in enumerate(pd.read_csv(path_to_csv, chunksize=100000, encoding='utf-8')):
-            with jsonlines.open(dir_to_save / f"{i}.jsonl", mode='w') as writer:
+        results_dir = Path(results_dir)
+        for i, chunk in enumerate(pd.read_csv(csv_file_path, chunksize=100000, encoding='utf-8')):
+            with jsonlines.open(results_dir / f"{i}.jsonl", mode='w') as writer:
                 for record in chunk.to_dict(orient='records'):
                     writer.write(record)
+
+    @staticmethod
+    def split_original_csv_file_by_date(
+        original_csv_path: str | Path,
+        train_data_start_date: int,
+        train_data_end_date: int,
+        train_data_path: str | Path,
+        val_data_path: str | Path,
+    ):
+        all_data_df = pd.read_csv(original_csv_path)
+        df = all_data_df[all_data_df['date'] >= train_data_start_date]
+        train_data_df = df[df['date'] <= train_data_end_date]
+        valid_data_df = df[df['date'] >= train_data_end_date]
+        train_data_df.to_json(train_data_path, orient='records', force_ascii=False)
+        valid_data_df.to_json(val_data_path, orient='records', force_ascii=False)
 
     # ====主要方法。====
     @staticmethod
     def train_val_split(
-        dir_to_jsonl_files: str | Path,
-        dir_to_results: str | Path,
+        jsonl_files_dir: str | Path,
+        results_dir: str | Path,
         start_time: int,
         end_time: int,
     ) -> None:
@@ -51,8 +66,8 @@ class PreProcessingMethods:
     # ====工具方法。====
     @staticmethod
     def filter_data_by_time(
-        dir_to_jsonl_files: str | Path,
-        dir_to_results: str | Path,
+        jsonl_files_dir: str | Path,
+        results_dir: str | Path,
         start_time: int,
         end_time: int,
     ) -> None:
@@ -63,21 +78,21 @@ class PreProcessingMethods:
             - date字段代表时间，数据类型是int。
 
         Args:
-            dir_to_jsonl_files (Union[str, Path]): 上一步数据的文件夹。
-            dir_to_results (Union[str, Path]): 要保存处理结果的文件夹。
+            jsonl_files_dir (Union[str, Path]): 上一步数据的文件夹。
+            results_dir (Union[str, Path]): 要保存处理结果的文件夹。
             start_time (int): 最早的时间。
             end_time (int): 最晚的时间。
 
         Returns:
             None: 复制文件上一步文件夹中的文件，保存筛选结果至指定文件夹。
         """
-        dir_to_jsonl_files = Path(dir_to_jsonl_files)
-        dir_to_results = Path(dir_to_results)
-        for i, item_path in enumerate(dir_to_jsonl_files.iterdir()):
+        jsonl_files_dir = Path(jsonl_files_dir)
+        results_dir = Path(results_dir)
+        for i, item_path in enumerate(jsonl_files_dir.iterdir()):
             if item_path.is_file() and item_path.suffix == '.jsonl':
                 df = pd.read_json(item_path, lines=True)
                 df_ = df[df['date'] >= start_time]
                 df_ = df_[df_['date'] <= end_time]
-                df_.to_json(dir_to_results / f"{i}.jsonl", orient='records', force_ascii=False)
-                print(f"Saved records in {dir_to_results / f'{i}.jsonl'}")
+                df_.to_json(results_dir / f"{i}.jsonl", orient='records', force_ascii=False)
+                print(f"Saved records in {results_dir / f'{i}.jsonl'}")
 
